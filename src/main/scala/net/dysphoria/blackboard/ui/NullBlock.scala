ab59410057bbeca4c4851303e377a5f1c802eec1
Andrew Forrest <andrew@dysphoria.net>
2009-06-08 14:14:25 +0100

    Oops, should have deleted these already.

--- src/main/scala/net/dysphoria/blackboard/ui/NullBlock.scala
+++
@@ -1,8 +0,0 @@
-package net.dysphoria.blackboard.ui
-
-import org.eclipse.swt.graphics._
-import blackboard.gfx._
-
-object NullBlock extends DisplayBlock {
-	def render(gfx: Gfx, position: Point) {}
-}
